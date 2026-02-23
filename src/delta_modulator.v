b6e3d845edd167055d4b3c7f06f207058921b49c
blu-exe <wowraphael@gmail.com>
2026-02-22 20:33:47 -0800

    Fixed lint error on output

--- src/delta_modulator.v
+++ src/delta_modulator.v
@@ -59,7 +59,9 @@ module tt_um_delta_modulator (
   // Enable input on uio_in
   assign uio_oe = 0;
 
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, uio_out[7:0], 1'b0};
+  assign uio_out = '0;
+  wire _unused = &{ena};
 
 endmodule
